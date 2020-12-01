90b67797dddf5b54a424444475d6cc9a190942e2
jogilsang <jogilsang@gmail.com>
2020-12-02 03:52:28 +0900

    added : hello, world

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -1,12 +1,11 @@
 package com.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("hello")
