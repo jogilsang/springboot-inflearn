ed08f9a39789b91085babe30402fbf34a8f46486
jogilsang <jogilsang@gmail.com>
2020-12-06 19:45:10 +0900

    added : comment to controller

--- src/main/java/com/springboot/controller/HelloController.java
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -11,24 +11,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    // url : http://localhost:8080/hello
+    // result :
+    // 안녕하세요, 손님
+    // message : hello, world!
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello, world!");
         return "hello";
     }
 
+    // url : http://localhost:8080/hello-mvc?name=jogilsang
+    // result :
+    // message : jogilsang
     @GetMapping("hello-mvc")
     public String hellomvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-mvc";
     }
 
+    // url : http://localhost:8080/hello-string?name=jogilsang
+    // result : hello jogilsang
     @GetMapping("hello-string")
     @ResponseBody
     public String hellostring(@RequestParam("name") String name) {
-        return "hello" + name;
+        return "hello " + name;
     }
 
+    // url : http://localhost:8080/hello-api?name=jogilsang
+    // result : {"name":"jogilsang"}
     @GetMapping("hello-api")
     @ResponseBody
     public Hello helloapi(@RequestParam("name") String name) {
