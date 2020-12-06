134cf4913b999cdaa06f81ff9540cc9dc4a16471
jogilsang <jogilsang@gmail.com>
2020-12-07 00:40:34 +0900

    added : jpa

--- src/main/java/com/springboot/SpringConfig.java
+++ src/main/java/com/springboot/SpringConfig.java
@@ -1,25 +1,29 @@
 package com.springboot;
 
 
-import com.springboot.repository.JdbcMemberRepository;
-import com.springboot.repository.JdbcTemplateMemberRepository;
-import com.springboot.repository.MemberRepository;
-import com.springboot.repository.MemoryMemberRepository;
+import com.springboot.repository.*;
 import com.springboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    //    private DataSource dataSource;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    // @Autowired
+    //    public SpringConfig(DataSource dataSource) {
+    //        this.dataSource = dataSource;
+    //    }
+
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -35,7 +39,8 @@ public class SpringConfig {
         // 다형성 활용
         // 인터페이스를 두고, 구현체를 바꿀수있음
 //       return new JdbcMemberRepository(dataSource);
-       return new JdbcTemplateMemberRepository(dataSource);
+//       return new JdbcTemplateMemberRepository(dataSource);
+       return new JpaMemberRepository(em);
     }
 
 }
--- src/main/java/com/springboot/service/MemberService.java
+++ src/main/java/com/springboot/service/MemberService.java
@@ -5,11 +5,13 @@ import com.springboot.repository.MemberRepository;
 import com.springboot.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 // @Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
