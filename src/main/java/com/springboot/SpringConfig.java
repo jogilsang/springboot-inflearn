f09ac6a219c6174550194a8bbabc016d94316bdb
jogilsang <jogilsang@gmail.com>
2020-12-28 00:21:54 +0900

    added : SpringDataJPA, but not execute

--- src/main/java/com/springboot/SpringConfig.java
+++ src/main/java/com/springboot/SpringConfig.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
@@ -20,28 +17,33 @@ public class SpringConfig {
     //        this.dataSource = dataSource;
     //    }
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
+    // private EntityManager em;
+
+    // 생성자가 하나인경우 생략가능
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+        // this.em = em;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-
-        // 객체지향적 설계
-        // 다형성 활용
-        // 인터페이스를 두고, 구현체를 바꿀수있음
-//       return new JdbcMemberRepository(dataSource);
-//       return new JdbcTemplateMemberRepository(dataSource);
-       return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // return new MemoryMemberRepository();
+//
+//        // 객체지향적 설계
+//        // 다형성 활용
+//        // 인터페이스를 두고, 구현체를 바꿀수있음
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JdbcTemplateMemberRepository(dataSource);
+//       // return new JpaMemberRepository(em);
+//    }
 
 }
--- src/main/java/com/springboot/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/com/springboot/repository/SpringDataJpaMemberRepository.java
@@ -2,12 +2,13 @@ package com.springboot.repository;
 
 import com.springboot.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
-// SpringDataJPA가 @bean없이 구현체를 자동으로 등록해줌
-public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository{
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member,
+        Long>, MemberRepository {
 
     @Override
     Optional<Member> findByName(String name);
+
 }
